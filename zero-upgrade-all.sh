12fb1801c4f74706f308c6f1967be12fd682fe7d
gear259 <51294291+gear259@users.noreply.github.com>
2019-08-01 23:10:21 +0100

    Update zero-upgrade-all.sh

--- zero-upgrade-all.sh
+++ zero-upgrade-all.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/sh
-
-
+!/usr/bin/sh
+######################################           script version             ##########################################
+######################################        zero generic upgrade.sh       ##########################################
 #wget -O /dev/null -q "http://localhost/web/message?text=WAIT WHILE USER FILES ARE BACKED UP TO HARD DRIVE&type=2&timeout=120" &
 #cp -f -v /etc/inadyn.conf /media/hdd & cp -f -v -r /etc/tuxbox/config /media/hdd & cp -f -v /etc/enigma2/autotimer.xml /media/hdd & cp -f -v /etc/enigma2/timers.xml /media/hdd & cp -f -v /etc/enigma2/#userbouquet.favourites.tv /media/hdd & cp -f -v /etc/fstab /media/hdd && cp -f -v /etc/network/interfaces /media/hdd & cp -f -v /etc/wpa_supplicant* /media/hdd & cp -f -v /etc/hostname /media/hdd &&
 #wget -O /dev/null -q "http://localhost/web/message?text=USER FILES BACKED UP, SAFE TO RUN UPDATE NEXT&type=2&timeout=3" &&
